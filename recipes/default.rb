914732cccf2a9e05ab6c912e3e5432c1af1c4a20
Umang Chouhan <uchouhan@optimiscorp.com>
2013-03-07 14:18:53 -0800

    Removing debug line.

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,8 +2,6 @@ raise 'No hosts entries found for node.' unless node['hosts']
 raise 'No hosts configured for this node.' if node['hosts'].empty?
 
 node['hosts'].each do |host|
-  Chef::Log.debug host.inspect
-
   hostsfile_entry host['ip'] do
     hostname host['name']
 
