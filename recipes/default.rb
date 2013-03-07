adc32b5ef80cda65c218302b46587e1541c70903
Umang Chouhan <uchouhan@optimiscorp.com>
2013-03-07 12:17:52 -0800

    Using #ip_address to set the IP.

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,6 +3,7 @@ raise 'No hosts configured for this node.' if node['hosts'].empty?
 
 node['hosts'].each do |host|
   hostsfile_entry host['ip'] do
+    ip_address host['ip']
     hostname host['name']
 
     aliases host['aliases'] if host['aliases']
