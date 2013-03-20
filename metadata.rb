d0c696546556e391f59fe5acbe2fd250c90ffb7e
Umang Chouhan <uchouhan@optimiscorp.com>
2013-03-20 15:24:37 -0700

    Updating the cookbook name.

--- metadata.rb
+++ metadata.rb
@@ -1,3 +1,5 @@
+name              'hosts'
+
 maintainer        'OptimisCorp., Inc.'
 maintainer_email  'ops+cookbooks@optimiscorp.com'
 
@@ -8,7 +10,7 @@ long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 
 version           '0.0.1'
 
-recipe            'chef-hostsfile::default', 'Configures /etc/hosts entries.'
+recipe            'hostsfile::default', 'Configures /etc/hosts entries.'
 
 %w{redhat centos scientific fedora debian ubuntu arch freebsd amazon}.each do |os|
   supports os
