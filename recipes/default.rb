2c816e1ea0cad32727db73cf608cc82bc0d0199c
shunwen <hsiaoshunwen@gmail.com>
2013-10-31 13:30:06 +0800

    Remove error for not setting node['hosts']

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,6 +1,4 @@
 raise 'No localhost entries found for node.' unless node['localhost']
-raise 'No hosts entries found for node.' unless node['hosts']
-raise 'No hosts configured for this node.' if node['hosts'].empty?
 
 localhost = node['localhost']
 hostsfile_entry localhost['ip'] do
@@ -10,12 +8,14 @@ hostsfile_entry localhost['ip'] do
   priority localhost['priority'] if localhost['priority']
 end
 
-node['hosts'].each do |host|
-  hostsfile_entry host['ip'] do
-    hostname host['name']
+if node['hosts']
+  node['hosts'].each do |host|
+    hostsfile_entry host['ip'] do
+      hostname host['name']
 
-    aliases host['aliases'] if host['aliases']
-    comment host['comment'] if host['comment']
-    priority host['priority'] if host['priority']
+      aliases host['aliases'] if host['aliases']
+      comment host['comment'] if host['comment']
+      priority host['priority'] if host['priority']
+    end
   end
 end
