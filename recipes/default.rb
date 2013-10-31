d2334765d959e0d0f1b3b24c7d161e1f5f414141
shunwen <hsiaoshunwen@gmail.com>
2013-10-31 13:26:46 +0800

    Separate localhost setting from 'hosts'

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,14 @@
+raise 'No localhost entries found for node.' unless node['localhost']
 raise 'No hosts entries found for node.' unless node['hosts']
 raise 'No hosts configured for this node.' if node['hosts'].empty?
+
+localhost = node['localhost']
+hostsfile_entry localhost['ip'] do
+  hostname localhost['name']
+  aliases localhost['aliases'] if localhost['aliases']
+  comment localhost['comment'] if localhost['comment']
+  priority localhost['priority'] if localhost['priority']
+end
 
 node['hosts'].each do |host|
   hostsfile_entry host['ip'] do
